7bb6bf2227cceeae1b4086802049f2973cc74aa9
Roberto Carlos Quintero <robertocarlero@gmail.com>
2025-10-11 21:36:38 -0400

    build: :heavy_plus_sign: Netlify integration added and setted

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 // @ts-check
@@ -5,9 +6,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	integrations: [react()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	output: 'server',
+	adapter: netlify({
+		edgeMiddleware: true,
+	}),
 });
