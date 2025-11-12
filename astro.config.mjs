a7eb94cf41bafae0157a45f9d19da343c72555cd
Encel Sanchez <esanchez@logoscorp.com>
2025-11-12 08:15:23 -0400

    chore: :mag: Astro integrations added for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+import robotsTxt from 'astro-robots-txt';
 // @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
+	site: 'https://homecinema.solutions',
+	integrations: [react(), sitemap(), robotsTxt()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
