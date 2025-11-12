708f3a45624b05f4fa557ea5626fb7ab30b16a7d
Encel Sanchez <esanchez@logoscorp.com>
2025-11-12 08:41:22 -0400

    chore: :zap: Compress integration added for improve the performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import playformCompress from '@playform/compress';
 import tailwindcss from '@tailwindcss/vite';
 import robotsTxt from 'astro-robots-txt';
 // @ts-check
@@ -9,7 +10,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://homecinema.solutions',
-	integrations: [react(), sitemap(), robotsTxt()],
+	integrations: [react(), sitemap(), robotsTxt(), playformCompress()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
